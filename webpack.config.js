779a29fdbc46d7186a1bfc6c6e99b4553b3b5b8e
Rom4eg <1969397@mail.ru>
2016-11-07 23:07:38 +0300

    added webpack config, rules for git to ignore builded static, changed django static root to build directory, added sample js controller

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const path = require('path');
+let base_dir = path.join(__dirname, 'myCompany');
+
+
+module.exports = {
+  context: path.join(base_dir, "myCompany/static/js/"),
+  entry: {
+    home: './home.js',
+  },
+  output: {
+    path: path.join(base_dir, "myCompany/static/build"),
+    filename: "[name].js"
+  }
+};
