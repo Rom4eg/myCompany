d8a498afc5fd8e1698d3fd1fe999c09e652233fd
Rom4eg <1969397@mail.ru>
2016-11-09 21:44:35 +0300

    responsive menu, complete foundation(until optimization stage)

--- myCompany/myCompany/static/js/base.js
+++ myCompany/myCompany/static/js/base.js
@@ -1,10 +1,16 @@
 
-import "foundation-sites/js/foundation.core";
-import "foundation-sites/js/foundation.util.keyboard";
-import "foundation-sites/js/foundation.util.box";
-import "foundation-sites/js/foundation.util.nest";
-import "foundation-sites/js/foundation.util.mediaQuery";
-import "foundation-sites/js/foundation.dropdownMenu";
+import "foundation-sites/dist/foundation";
+// import "foundation-sites/js/foundation.core";
+// import "foundation-sites/js/foundation.util.keyboard";
+// import "foundation-sites/js/foundation.util.box";
+// import "foundation-sites/js/foundation.util.nest";
+// import "foundation-sites/js/foundation.util.mediaQuery";
+// import "foundation-sites/js/foundation.dropdownMenu";
+// import "foundation-sites/js/foundation.util.triggers";
+// import "foundation-sites/js/foundation.accordion";
+// import "foundation-sites/js/foundation.responsiveMenu";
+
+
 export class BaseController{
 
   constructor(){
