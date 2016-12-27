63d22aa3d8b914e22eedcc1199e4adbc152c8e7c
Rom4eg <1969397@mail.ru>
2016-12-27 23:52:50 +0300

    update password template

--- myCompany/web/views/__init__.py
+++ myCompany/web/views/__init__.py
@@ -6,7 +6,7 @@ from web.views.employees import Employees
 from web.views.common import ApiScheme
 from web.views.auth import Login
 from web.views.auth import Registration
-from web.views.auth import ResetPassword
+from web.views.auth import ResetPwd
 
 __all__ = ['Dashboard', 'Rules', 'EventsList', 'Employees', 'ApiScheme', 'Login'
-            'Registration', 'ResetPassword']
+            'Registration', 'ResetPwd']
--- myCompany/web/views/auth.py
+++ myCompany/web/views/auth.py
@@ -1,4 +1,8 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render_to_response
+from django.core.exceptions import ObjectDoesNotExist
+from users.models import ResetPassword
+
 
 class Login(TemplateView):
 
@@ -8,10 +12,18 @@ class Registration(TemplateView):
 
     template_name = "users/auth.html"
 
-class ResetPassword(TemplateView):
+class ResetPwd(TemplateView):
 
     template_name = "users/auth.html"
 
-    def get(self, request):
-        print(request)
-        return super(ResetPassword, self).get(request)
+    def get(self, request, reset_hash=None):
+        if not reset_hash:
+            return super(ResetPassword, self).get(request)
+        try:
+            pwd_hash = ResetPassword.objects.get(pwd_hash=reset_hash)
+        except ObjectDoesNotExist as err:
+            return super(ResetPassword, self).get(request)
+        return render_to_response("users/update_pwd.html", {"request": request, "user": pwd_hash.user})
+
+    def put(self, request):
+        pass
